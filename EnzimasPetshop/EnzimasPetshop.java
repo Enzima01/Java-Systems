9c70c2fd175e9e1f28bfee66f3da239f6394c98b
Enzo Henrique Favaro <enzo.h.favaro@gmail.com>
2025-05-02 19:48:29 -0300

    EnzimasPetshop.java

--- EnzimasPetshop/EnzimasPetshop.java
+++ EnzimasPetshop/EnzimasPetshop.java
@@ -7,17 +7,17 @@ public class EnzimasPetshop {
 		char resp = 'S';
 		System.out.println("==== ENZIMA'S PETSHOP ====");
 		do {
-			Animal a1 = new Animal();
+			Animal x = new Animal();
 			System.out.print("Digite o nome de seu animal: ");
-			a1.nome = sc.nextLine();
+			x.nome = sc.nextLine();
 			System.out.print("Digite o tipo de seu animal: ");
-			a1.tipo = sc.nextLine();
+			x.tipo = sc.nextLine();
 			System.out.print("Digite a idade de seu animal: ");
-			a1.idade = sc.nextInt();
+			x.idade = sc.nextInt();
 			sc.nextLine();
 
-			a1.exibirDados();
-			a1.falar();
+			x.exibirDados();
+			x.falar();
 			System.out.print("Deseja repetir?(S/N): ");
 			resp = sc.nextLine().charAt(0);
 			while(resp != 'S' && resp != 'N') {
